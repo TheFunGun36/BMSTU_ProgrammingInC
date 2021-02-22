6e8fc6c27948425560e886226a250eaa0676c35a
Cheprasov Kirill <1149-chkm20u153@users.noreply.git.iu7.bmstu.ru>
2021-02-22 15:05:00 +0300

    First try for fifth task using euclid`s algorythm

---
+++ lab_01_05_02/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int biggest_common_factor(int a, int b)
+{
+    // Using euclid's algorythm
+
+    while (a != b)
+    {
+        if (a < b)
+            swap(&a, &b);
+
+        a -= b;
+    }
+
+    return a;
+}
+
+int main()
+{
+    int a, b;
+
+    int succesfully_read = scanf("%d%d", &a, &b);
+
+    if (succesfully_read != 2 || a <= 0 || b <= 0)
+    {
+        return -1;
+    }
+    else
+    {
+        int result = biggest_common_factor(a, b);
+
+        printf("%d", result);
+    }
+
+    return 0;
+}
