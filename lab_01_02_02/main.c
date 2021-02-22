52e1eb11d00ef90052698c9cc547f15380e0c1db
Cheprasov Kirill <1149-chkm20u153@users.noreply.git.iu7.bmstu.ru>
2021-02-22 12:46:46 +0300

    testing test system

---
+++ lab_01_02_02/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double distance(double pointa_x, double pointa_y, double pointb_x, double pointb_y)
+{
+    double distance_x = abs(pointa_x - pointb_x);
+    double distance_y = abs(pointa_y - pointb_y);
+    return sqrt(distance_x*distance_x + distance_y*distance_y); 
+}
+
+int main()
+{
+    double point1_x, point1_y;
+    double point2_x, point2_y;
+    double point3_x, point3_y;
+
+    printf("info\n");
+
+    scanf("%lf%lf", &point1_x, &point1_y);
+    scanf("%lf%lf", &point2_x, &point2_y);
+    scanf("%lf%lf", &point3_x, &point3_y);
+
+    double perimeter = 0.0;
+    perimeter += distance(point1_x, point1_y, point2_x, point2_y);
+    perimeter += distance(point1_x, point1_y, point3_x, point3_y);
+    perimeter += distance(point2_x, point2_y, point3_x, point3_y);
+
+    printf("%lf\n", perimeter);
+    
+    return 0;
+}
