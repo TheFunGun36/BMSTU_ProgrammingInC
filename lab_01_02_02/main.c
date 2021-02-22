b3a6c16a718d0daf04b109c470c9b9fdcd95812b
Cheprasov Kirill <1149-chkm20u153@users.noreply.git.iu7.bmstu.ru>
2021-02-22 12:54:01 +0300

    First actual try

--- lab_01_02_02/main.c
+++ lab_01_02_02/main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-double distance(double pointa_x, double pointa_y, double pointb_x, double pointb_y)
+/*double distance(double pointa_x, double pointa_y, double pointb_x, double pointb_y)
 {
     double distance_x = abs(pointa_x - pointb_x);
     double distance_y = abs(pointa_y - pointb_y);
-    return sqrt(distance_x*distance_x + distance_y*distance_y); 
-}
+    return sqrt(distance_x * distance_x + distance_y * distance_y); 
+}*/
 
 int main()
 {
@@ -22,9 +22,22 @@ int main()
     scanf("%lf%lf", &point3_x, &point3_y);
 
     double perimeter = 0.0;
-    perimeter += distance(point1_x, point1_y, point2_x, point2_y);
+
+    double distance_x = abs(point1_x - point2_x);
+    double distance_y = abs(point1_y - point2_y);
+    perimeter += sqrt(distance_x * distance_x + distance_y + distance_y);
+
+    distance_x = abs(point2_x - point3_x);
+    distance_y = abs(point2_y - point3_y);
+    perimeter += sqrt(distance_x * distance_x + distance_y + distance_y);
+
+    distance_x = abs(point1_x - point3_x);
+    distance_y = abs(point1_y - point3_y);
+    perimeter += sqrt(distance_x * distance_x + distance_y + distance_y);
+
+    /*perimeter += distance(point1_x, point1_y, point2_x, point2_y);
     perimeter += distance(point1_x, point1_y, point3_x, point3_y);
-    perimeter += distance(point2_x, point2_y, point3_x, point3_y);
+    perimeter += distance(point2_x, point2_y, point3_x, point3_y);*/
 
     printf("%lf\n", perimeter);
     
