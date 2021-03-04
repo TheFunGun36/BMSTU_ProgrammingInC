28c65841e4a6c74dc0f213effee9d49e1deda7b0
Cheprasov Kirill <chkm20u153@student.bmstu.ru>
2021-03-04 15:52:40 +0300

    task 6: fixed wrong sigh horizontal line

--- lab_01_06_03/main.c
+++ lab_01_06_03/main.c
@@ -66,7 +66,7 @@ int main()
         // vertical
         if (equal(point_x, line_point1_x))
             result = 1;
-        else if (point_x > line_point1_x)
+        else if (point_x < line_point1_x)
             result = 2;
         result = 0;
         //return -1;
