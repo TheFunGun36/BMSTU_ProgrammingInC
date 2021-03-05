505600cad7618b4b44f91bd1f547605ed4329ff3
Jora <maki-333@yandex.ru>
2021-03-05 15:29:43 +0300

    task 7: refactored

--- lab_01_07_03/main.c
+++ lab_01_07_03/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
-int equal(double value_a, double value_b)
+#define EXIT_OK 0
+#define EXIT_INVALID_INPUT -1
+#define EXIT_INVALID_EPS -2
+#define EXIT_INVALID_X -3
+
+int is_equal(double value_a, double value_b);
+double calculate_f(double x);
+double calculate_s(double x, double e);
+double abs_delta(double value1, double value2);
+double rel_delta(double abs_delta, double value);
+
+int main()
+{
+    double x;
+    double e;
+    
+    if (scanf("%lf%lf", &x, &e) != 2)
+        return EXIT_INVALID_INPUT;
+
+    if (e > 1.0 || e <= 0.0 || is_equal(e, 0.0))
+        return EXIT_INVALID_EPS;
+
+    if (fabs(x) > 1.0)
+        return EXIT_INVALID_X;
+    
+    double f_value = calculate_f(x);
+    double s_value = calculate_s(x, e);
+    double abs_d = abs_delta(f_value, s_value);
+    double rel_d = rel_delta(abs_d, f_value);
+
+    printf("%lf %lf %lf %lf", f_value, s_value, abs_d, rel_d);
+    
+    return EXIT_OK;
+}
+
+int is_equal(double value_a, double value_b)
 {
     return fabs(value_a - value_b) <= 1e-10;
 }
@@ -41,28 +76,4 @@ double abs_delta(double value1, double value2)
 double rel_delta(double abs_delta, double value)
 {
     return abs_delta / fabs(value);
-}
-
-int main()
-{
-    double x;
-    double e;
-    
-    if (scanf("%lf%lf", &x, &e) != 2)
-        return -1;
-
-    if (e > 1.0 || e <= 0.0 || equal(e, 0.0))
-        return -2;
-
-    if (fabs(x) > 1.0)
-        return -3;
-    
-    double f_value = calculate_f(x);
-    double s_value = calculate_s(x, e);
-    double abs_d = abs_delta(f_value, s_value);
-    double rel_d = rel_delta(abs_d, f_value);
-
-    printf("%lf %lf %lf %lf", f_value, s_value, abs_d, rel_d);
-    
-    return 0;
 }
