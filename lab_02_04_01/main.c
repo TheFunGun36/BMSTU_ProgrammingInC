3589bb218ab3c2437e1830ad9336be0590aa1bd1
Cheprasov Kirill <chkm20u153@student.bmstu.ru>
2021-03-14 18:53:47 +0300

    lab 2, task 4: reverted empty array check

--- lab_02_04_01/main.c
+++ lab_02_04_01/main.c
@@ -32,6 +32,8 @@ int get_array_from_user(int *array, int *arr_size)
 
         if (result != 1)
         {
+            if (i == 0)
+                return NO_ELEMENTS;
 
             *arr_size = i;
             return OK;
