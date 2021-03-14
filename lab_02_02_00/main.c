d600669fd184755cc70b31583af4c56c820cd72e
Cheprasov Kirill <chkm20u153@student.bmstu.ru>
2021-03-14 16:57:45 +0300

    Now algorithm uses only one array

--- lab_02_02_00/main.c
+++ lab_02_02_00/main.c
@@ -7,23 +7,22 @@
 
 int get_array_from_user(int *array, int arr_size);
 double get_arithmetic_mean(int *array, int arr_size);
-void form_new_array(int *source, int src_size, int *destination, int *dest_size);
+void reform_array(int *array, int src_size, int *dest_size);
 void print_array(int *array, int arr_size);
 
 int main()
 {
-    int input_array[ARR_SIZE];
-    int exit_code = get_array_from_user(input_array, ARR_SIZE);
+    int array[ARR_SIZE];
+    int exit_code = get_array_from_user(array, ARR_SIZE);
 
     if (exit_code != OK)
         return exit_code;
 
-    int result_array[ARR_SIZE];
     int new_array_size;
 
-    form_new_array(input_array, ARR_SIZE, result_array, &new_array_size);
+    reform_array(array, ARR_SIZE, &new_array_size);
 
-    print_array(result_array, new_array_size);
+    print_array(array, new_array_size);
 
     return 0;
 }
@@ -49,18 +48,17 @@ double get_arithmetic_mean(int *array, int arr_size)
     return result / arr_size;
 }
 
-void form_new_array(int *source, int src_size, int *destination, int *dest_size)
+void reform_array(int *array, int src_size, int *dest_size)
 {
-    double arithmetic_mean = get_arithmetic_mean(source, src_size);
+    double arithmetic_mean = get_arithmetic_mean(array, src_size);
     *dest_size = 0;
 
     for (int i = 0; i < src_size; i++)
     {
-        if (source[i] > arithmetic_mean)
+        if (array[i] > arithmetic_mean)
         {
+            array[*dest_size] = array[i];
             (*dest_size)++;
-            *destination = source[i];
-            destination++;
         }
     }
 }
