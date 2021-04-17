49207e23693ac720e7695575cf449868d74dd114
Cheprasov Kirill <chkm20u153@student.bmstu.ru>
2021-04-17 08:58:54 +0300

    lab 2, task 2: now program forming new array, instead of modifying old one

--- lab_02_02_00/main.c
+++ lab_02_02_00/main.c
@@ -8,8 +8,8 @@
 #define NO_ELEMENTS_IN_NEW_ARRAY -3
 
 int get_array_from_user(int *array, int *arr_size);
-double get_arithmetic_mean(int *array, int arr_size);
-void reform_array(int *array, int *arr_size);
+double get_arithmetic_mean(const int *array, int arr_size);
+void form_new_array(const int *array, int arr_size, int *new_array, int *new_arr_size);
 void print_array(int *array, int arr_size);
 
 int main()
@@ -21,12 +21,14 @@ int main()
     if (exit_code != OK)
         return exit_code;
 
-    reform_array(array, &array_size);
+    int new_array[ARR_SIZE];
+    int new_array_size;
+    form_new_array(array, array_size, new_array, &new_array_size);
 
     if (array_size <= 0)
         return NO_ELEMENTS_IN_NEW_ARRAY;
 
-    print_array(array, array_size);
+    print_array(new_array, new_array_size);
 
     return exit_code;
 }
@@ -48,7 +50,7 @@ int get_array_from_user(int *array, int *arr_size)
     return OK;
 }
 
-double get_arithmetic_mean(int *array, int arr_size)
+double get_arithmetic_mean(const int *array, int arr_size)
 {
     double result = 0.0;
 
@@ -58,21 +60,21 @@ double get_arithmetic_mean(int *array, int arr_size)
     return result / arr_size;
 }
 
-void reform_array(int *array, int *arr_size)
+void form_new_array(const int *array, int arr_size, int *new_array, int *new_arr_size)
 {
-    double arithmetic_mean = get_arithmetic_mean(array, *arr_size);
+    double arithmetic_mean = get_arithmetic_mean(array, arr_size);
     int new_size = 0;
 
-    for (int i = 0; i < *arr_size; i++)
+    for (int i = 0; i < arr_size; i++)
     {
         if (array[i] > arithmetic_mean)
         {
-            array[new_size] = array[i];
+            new_array[new_size] = array[i];
             new_size++;
         }
     }
 
-    *arr_size = new_size;
+    *new_arr_size = new_size;
 }
 
 void print_array(int *array, int arr_size)
