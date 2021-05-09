13be2bf7aac16a49700b49abe9fe7b74daba7dfc
Jora <maki-333@yandex.ru>
2021-05-09 20:54:13 +0300

    lab 4, task 4: changed input style

--- lab_04_03_00/main.c
+++ lab_04_03_00/main.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-//#include "word_array.h"
 #include "word_array_works.h"
 
 #define DEBUG
 
 int get_string_from_user(char *string, int max_length)
 {
-    fgets(string, max_length + 2, stdin);
-    string[max_length + 1] = '\0';
+    if (!fgets(string, max_length + 2, stdin))
+        return UNEXPECTED_INPUT_ERROR;
 
-    int str_length = strlen(string);
+    int len = strlen(string);
 
-    if (str_length > max_length)
+    if (len >= max_length + 1 && string[len - 1] != '\n')
         return STRING_IS_TOO_LONG;
-    if (str_length <= 0)
-        return STRING_IS_TOO_SHORT;
 
-    if (string[str_length - 1] == '\n')
-        string[str_length - 1] = '\0';
+    string[len - 1] = '\0';
 
     return OK;
 }
 
 int main()
 {
-    char input_str[MAX_STRING_LENGTH + 1];
+    char input_str[MAX_STRING_LENGTH + 2];
 
     int exit_code = get_string_from_user(input_str, MAX_STRING_LENGTH);
 
-    if (exit_code != OK)
-    {
-        return exit_code;
-    }
-    else
+    if (exit_code == OK)
     {
         char str_arr[MAX_WORD_AMOUNT][MAX_WORD_LENGTH + 1];
         int arr_size;
@@ -43,21 +35,22 @@ int main()
 
         exit_code = pop_last_word(input_str_ptr, &word_to_exclude);
 
-        if (exit_code != OK)
-            return exit_code;
+        if (exit_code == OK)
+        {
+            exit_code = split_string(input_str, word_to_exclude, str_arr, &arr_size);
 
-        exit_code = split_string(input_str, word_to_exclude, str_arr, &arr_size);
+            if (!arr_size)
+                exit_code = EMPTY_RESULT_ARRAY;
 
-        if (exit_code != OK)
-            return exit_code;
+            if (exit_code == OK)
+            {
+                remove_first_letter_entry(str_arr, arr_size);
+                reverse_word_array(str_arr, arr_size);
+                strarr_to_string(str_arr, arr_size, ' ', input_str);
 
-        remove_first_letter_entry(str_arr, arr_size);
-        reverse_word_array(str_arr, arr_size);
-
-        strarr_to_string(str_arr, arr_size, ' ', input_str);
-
-        printf("Result: %s\n", input_str);
-        //aaaaaaaaa abc abc def asldfkj abc def ded def
+                printf("Result: %s\n", input_str);
+            }
+        }
     }
 
     return exit_code;
