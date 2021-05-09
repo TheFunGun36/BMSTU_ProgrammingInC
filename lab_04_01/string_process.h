9049d48760b811313559720bf3f2ca03577e56ff
Jora <maki-333@yandex.ru>
2021-05-09 20:53:43 +0300

    lab 4, task 1: removed 'const'

--- lab_04_01/string_process.h
+++ lab_04_01/string_process.h
@@ -1,10 +1,10 @@
 #ifndef STRING_PROCESS_H
 #define STRING_PROCESS_H
 
-const char *my_strpbrk(const char *main_str, const char *small_str);
-unsigned int my_strspn(const char *main_str, const char *symbol_set);
-unsigned int my_strcspn(const char *main_str, const char *symbol_set);
-const char *my_strchr(const char *string, int symbol);
-const char *my_strrchr(const char *string, int symbol);
+char *my_strpbrk(char *main_str, char *small_str);
+unsigned int my_strspn(char *main_str, char *symbol_set);
+unsigned int my_strcspn(char *main_str, char *symbol_set);
+char *my_strchr(char *string, int symbol);
+char *my_strrchr(char *string, int symbol);
 
 #endif
