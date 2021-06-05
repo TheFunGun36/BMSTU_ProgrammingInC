36eb3eebeb1437526a8f1cf6f102c90be7c8698a
Cheprasov Kirill <cheprasovkm@student.bmstu.ru>
2021-06-05 10:07:15 +0300

    lab 3, task 5: now program forming array and interpreting matrix as matrix

--- lab_03_05_00/main.c
+++ lab_03_05_00/main.c
@@ -9,24 +9,21 @@
 #define INVALID_MATRIX_ELEMENT -3
 #define NO_PRIME_NUMBERS -4
 
-int get_matrix_from_user(int *matrix, size_t *size, size_t *size_x)
+int get_matrix_from_user(int matrix[][MAX_MATRIX_SIZE_X], size_t *size_x, size_t *size_y)
 {
+    if (scanf("%lud", size_y) != 1 || *size_y <= 0 || *size_y > MAX_MATRIX_SIZE_Y)
+        return INVALID_SZ_Y;
+
+    if (scanf("%lud", size_x) != 1 || *size_x <= 0 || *size_x > MAX_MATRIX_SIZE_X)
+        return INVALID_SZ_X;
+
+    for (size_t i = 0; i < *size_y; i++)
     {
-        size_t size_y;
-        
-        if (scanf("%lud", &size_y) != 1 || size_y <= 0 || size_y > MAX_MATRIX_SIZE_Y)
-            return INVALID_SZ_Y;
-
-        if (scanf("%lud", size_x) != 1 || *size_x <= 0 || *size_x > MAX_MATRIX_SIZE_X)
-            return INVALID_SZ_X;
-
-        *size = *size_x * size_y;
-    }
-
-    for (unsigned int i = 0; i < *size; i++)
-    {
-        if (scanf("%d", &matrix[i]) != 1)
-            return INVALID_MATRIX_ELEMENT;
+        for (size_t j = 0; j < *size_x; j++)
+        {
+            if (scanf("%d", &matrix[i][j]) != 1)
+                return INVALID_MATRIX_ELEMENT;
+        }
     }
 
     return OK;
@@ -44,15 +41,6 @@ int is_prime(int value)
     return 1;
 }
 
-int array_has_primes(int *array, size_t size)
-{
-    for (int i = 0; i < size; i++)
-        if (is_prime(array[i]))
-            return 1;
-
-    return 0;
-}
-
 void swap(int *a, int *b)
 {
     int tmp = *a;
@@ -60,57 +48,86 @@ void swap(int *a, int *b)
     *b = tmp;
 }
 
-void reconstruct_matrix(int *matrix, size_t size)
+void form_prime_array(int matrix[][MAX_MATRIX_SIZE_X], size_t size_x, size_t size_y, int prime_array[], size_t *arr_size)
 {
-    int *prime1 = matrix;
-    int *prime2 = matrix + size;
+    size_t new_array_size = 0;
 
-    while (prime1 < prime2)
+    for (size_t i = 0; i < size_y; i++)
     {
-        do
-            prime1++;
-        while (prime1 < prime2 && !is_prime(*prime1));
-        
-        do
-            prime2--;
-        while (prime1 < prime2 && !is_prime(*prime2));
+        for (size_t j = 0; j < size_x; j++)
+        {
+            if (is_prime(matrix[i][j]))
+            {
+                prime_array[new_array_size] = matrix[i][j];
+                matrix[i][j] = 2;
+                new_array_size++;
+            }
+        }
+    }
 
-        if (prime1 < prime2)
-            swap(prime1, prime2);
+    *arr_size = new_array_size;
+}
+
+void reverse_array(int array[], int array_size)
+{
+    int *first = array;
+    int *last = array + array_size - 1;
+
+    while (first < last)
+    {
+        swap(first++, last--);
     }
 }
 
-void print_matrix(int *matrix, size_t size, size_t size_x)
+void put_array_in_matrix(int matrix[][MAX_MATRIX_SIZE_X], size_t size_x, size_t size_y, int *prime_array)
 {
-    for (unsigned int i = 0; i < size; i++)
+    for (size_t i = 0; i < size_y; i++)
     {
-        printf("%d", matrix[i]);
+        for (size_t j = 0; j < size_x; j++)
+        {
+            if (matrix[i][j] == 2)
+                matrix[i][j] = *prime_array++;
+        }
+    }
+}
 
-        if ((i + 1) % size_x)
-            printf(" ");
-        else
-            printf("\n");    
+void print_matrix(int matrix[][MAX_MATRIX_SIZE_X], size_t size_x, size_t size_y)
+{
+    for (size_t i = 0; i < size_y; i++)
+    {
+        for (size_t j = 0; j < size_x; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+
+        printf("\n");
     }
 }
 
 int main()
 {
-    int array[MAX_MATRIX_SIZE_Y * MAX_MATRIX_SIZE_X];
-    size_t array_size, matrix_size_x;
+    int matrix[MAX_MATRIX_SIZE_Y][MAX_MATRIX_SIZE_X];
+    size_t matrix_size_x, matrix_size_y;
 
-    int exit_code = get_matrix_from_user(array, &array_size, &matrix_size_x);
+    int prime_array[MAX_MATRIX_SIZE_X * MAX_MATRIX_SIZE_Y];
+    size_t prime_array_size;
+
+    int exit_code = get_matrix_from_user(matrix, &matrix_size_x, &matrix_size_y);
 
     if (exit_code != OK)
         return exit_code;
 
-    if (array_has_primes(array, array_size))
+    form_prime_array(matrix, matrix_size_x, matrix_size_y, prime_array, &prime_array_size);
+
+    if (prime_array_size <= 0)
     {
-        reconstruct_matrix(array, array_size);
-        print_matrix(array, array_size, matrix_size_x);
+        exit_code = NO_PRIME_NUMBERS;
     }
     else
     {
-        exit_code = NO_PRIME_NUMBERS;
+        reverse_array(prime_array, prime_array_size);
+        put_array_in_matrix(matrix, matrix_size_x, matrix_size_y, prime_array);
+        print_matrix(matrix, matrix_size_x, matrix_size_y);
     }
 
     return exit_code;
