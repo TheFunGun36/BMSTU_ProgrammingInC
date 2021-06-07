88c70dac696dcdaeba48b0601748c7b52a94a76f
Cheprasov Kirill <cheprasovkm@student.bmstu.ru>
2021-06-07 10:57:04 +0300

    lab 4, task 2: decomposed main(), enhanced naming and sort function

--- lab_04_02_01/string_part.c
+++ lab_04_02_01/string_part.c
@@ -9,9 +9,9 @@ char *get_str_end(char *str)
     return str;
 }
 
-string_part get_empty_string()
+string_part_t get_empty_string()
 {
-    string_part str;
+    string_part_t str;
 
     str.begin = (char*)0x0;
     str.end = str.begin;
@@ -19,7 +19,7 @@ string_part get_empty_string()
     return str;
 }
 
-int is_char_in_string(string_part string, char chr)
+int is_char_in_string(string_part_t string, char chr)
 {
     while (string.begin < string.end)
     {
@@ -32,9 +32,9 @@ int is_char_in_string(string_part string, char chr)
     return 0;
 }
 
-string_part get_word(string_part source_str, int word_count, string_part splitter_set)
+string_part_t get_word(string_part_t source_str, int word_count, string_part_t splitter_set)
 {
-    string_part word;
+    string_part_t word;
     word.begin = source_str.begin;
     word.end = word.begin;
 
@@ -59,9 +59,9 @@ string_part get_word(string_part source_str, int word_count, string_part splitte
     return get_empty_string();
 }
 
-string_part find_word(string_part source_str, string_part word, string_part splitter_set)
+string_part_t find_word(string_part_t source_str, string_part_t word, string_part_t splitter_set)
 {
-    string_part current_word;
+    string_part_t current_word;
 
     for (int i = 1; source_str.begin < source_str.end; i++)
     {
@@ -76,13 +76,13 @@ string_part find_word(string_part source_str, string_part word, string_part spli
     return get_empty_string();
 }
 
-int count_word(string_part source_str, string_part word, string_part splitter_set)
+int count_word(string_part_t source_str, string_part_t word, string_part_t splitter_set)
 {
     int words_found = 0;
 
     for (int i = 1; source_str.begin < source_str.end; i++)
     {
-        string_part current_word = get_word(source_str, 0, splitter_set);
+        string_part_t current_word = get_word(source_str, 0, splitter_set);
         words_found += is_strings_equal(current_word, word);
         move_beg_one_word_forward(&source_str, splitter_set);
     }
@@ -90,18 +90,18 @@ int count_word(string_part source_str, string_part word, string_part splitter_se
     return words_found;
 }
 
-void print_string(string_part str)
+void print_string(string_part_t str)
 {
     while (str.begin < str.end)
         printf("%c", *(str.begin)++);
 }
 
-int is_string_empty(string_part str)
+int is_string_empty(string_part_t str)
 {
     return str.begin == str.end;
 }
 
-int is_strings_equal(string_part str1, string_part str2)
+int is_strings_equal(string_part_t str1, string_part_t str2)
 {
     if (str1.end - str1.begin == str2.end - str2.begin)
     {
@@ -113,16 +113,16 @@ int is_strings_equal(string_part str1, string_part str2)
     return 0;
 }
 
-string_part get_full_str_part(char *str)
+string_part_t get_full_str_part(char *str)
 {
-    string_part my_str;
+    string_part_t my_str;
     my_str.begin = str;
     my_str.end = get_str_end(str);
 
     return my_str;
 }
 
-void move_beg_one_word_forward(string_part *str, string_part splitter_set)
+void move_beg_one_word_forward(string_part_t *str, string_part_t splitter_set)
 {
     str->begin = get_word(*str, 1, splitter_set).begin;
 
@@ -130,12 +130,12 @@ void move_beg_one_word_forward(string_part *str, string_part splitter_set)
         str->begin = str->end;
 }
 
-int get_str_size(string_part str)
+int get_str_size(string_part_t str)
 {
     return str.end - str.begin;
 }
 
-int get_word_max_len(string_part str, string_part splitter_set)
+int get_word_max_len(string_part_t str, string_part_t splitter_set)
 {
     int word_size_max = 0;
     while (str.begin < str.end)
@@ -151,10 +151,10 @@ int get_word_max_len(string_part str, string_part splitter_set)
     return word_size_max;
 }
 
-void split_into_word_array(string_part str, string_part splitter_set, int arr_len_max, string_part array[], int *array_len)
+void split_into_word_array(string_part_t str, string_part_t splitter_set, int arr_len_max, string_part_t array[], int *array_len)
 {
     *array_len = 0;
-    string_part word = get_word(str, 0, splitter_set);
+    string_part_t word = get_word(str, 0, splitter_set);
 
     while (!is_string_empty(word) && *array_len < arr_len_max)
     {
@@ -165,11 +165,11 @@ void split_into_word_array(string_part str, string_part splitter_set, int arr_le
     }
 }
 
-void split_into_word_set(string_part str, string_part splitter_set, int arr_len_max, string_part array[], int *array_len)
+void split_into_word_set(string_part_t str, string_part_t splitter_set, int arr_len_max, string_part_t array[], int *array_len)
 {
     *array_len = 0;
-    string_part word = get_word(str, 0, splitter_set);
-    string_part left_part = str;
+    string_part_t word = get_word(str, 0, splitter_set);
+    string_part_t left_part = str;
     left_part.end = str.begin;
 
     while (!is_string_empty(word) && *array_len < arr_len_max)
@@ -187,10 +187,10 @@ void split_into_word_set(string_part str, string_part splitter_set, int arr_len_
     }
 }
 
-string_part get_larger_string(string_part str1, string_part str2)
+string_part_t get_larger_string(string_part_t str1, string_part_t str2)
 {
-    string_part str1_saved = str1;
-    string_part str2_saved = str2;
+    string_part_t str1_saved = str1;
+    string_part_t str2_saved = str2;
 
     while (str1.begin < str1.end && str2.begin < str2.end)
     {
@@ -212,4 +212,28 @@ string_part get_larger_string(string_part str1, string_part str2)
         return str2_saved;
 
     return get_empty_string();
+}
+
+int string_compare(string_part_t str1, string_part_t str2)
+{
+    while (str1.begin < str1.end && str2.begin < str2.end)
+    {
+        if (*str1.begin != *str2.begin)
+        {
+            if (*str1.begin > *str2.begin)
+                return 1;
+            else
+                return -1;
+        }
+
+        str1.begin++;
+        str2.begin++;
+    }
+
+    if (!is_string_empty(str1))
+        return -1;
+    else if (!is_string_empty(str2))
+        return 1;
+
+    return 0;
 }
--- lab_04_02_01/string_part.h
+++ lab_04_02_01/string_part.h
@@ -1,25 +1,26 @@
 #ifndef STRING_PART_H
 #define STRING_PART_H
 
-typedef struct string_part
+typedef struct string_part_t
 {
     char *begin;
     char *end;
-} string_part;
+} string_part_t;
 
-string_part get_empty_string();
-string_part get_word(string_part source_str, int word_index, string_part splitter_set);
-string_part find_word(string_part source_str, string_part word, string_part splitter_set);
-int count_word(string_part source_str, string_part word, string_part splitter_set);
-void print_string(string_part str);
-int is_string_empty(string_part str);
-int is_strings_equal(string_part str1, string_part str2);
-string_part get_full_str_part(char *str);
-void move_beg_one_word_forward(string_part *str, string_part splitter_set);
-int get_str_size(string_part str);
-int get_word_max_len(string_part str, string_part splitter_set);
-void split_into_word_array(string_part str, string_part splitter_set, int arr_len_max, string_part array[], int *array_len);
-void split_into_word_set(string_part str, string_part splitter_set, int arr_len_max, string_part array[], int *array_len);
-string_part get_larger_string(string_part str1, string_part str2);
+string_part_t get_empty_string();
+string_part_t get_word(string_part_t source_str, int word_index, string_part_t splitter_set);
+string_part_t find_word(string_part_t source_str, string_part_t word, string_part_t splitter_set);
+int count_word(string_part_t source_str, string_part_t word, string_part_t splitter_set);
+void print_string(string_part_t str);
+int is_string_empty(string_part_t str);
+int is_strings_equal(string_part_t str1, string_part_t str2);
+string_part_t get_full_str_part(char *str);
+void move_beg_one_word_forward(string_part_t *str, string_part_t splitter_set);
+int get_str_size(string_part_t str);
+int get_word_max_len(string_part_t str, string_part_t splitter_set);
+void split_into_word_array(string_part_t str, string_part_t splitter_set, int arr_len_max, string_part_t array[], int *array_len);
+void split_into_word_set(string_part_t str, string_part_t splitter_set, int arr_len_max, string_part_t array[], int *array_len);
+string_part_t get_larger_string(string_part_t str1, string_part_t str2);
+int string_compare(string_part_t str1, string_part_t str2);
 
 #endif
