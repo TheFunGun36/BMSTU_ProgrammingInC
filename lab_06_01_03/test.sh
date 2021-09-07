f636fb605b133f5b482bc5edd159ecc02a4fbb35
Cheprasov Kirill <cheprasovkm@student.bmstu.ru>
2021-09-07 23:43:58 +0300

    lab6: added some initial tests, created test.sh script

---
+++ lab_06_01_03/test.sh
@@ -0,0 +1,41 @@
+POSITIVES_SUCCEEDED=0
+POSITIVES_SUM=0
+NEGATIVES_SUCCEEDED=0
+NEGATIVES_SUM=0
+
+for file in `ls func_tests`
+do
+    if [[ $file =~ pos_[0-9]{2}_in.txt ]]
+    then
+        ((POSITIVES_SUM++))
+        i=${file%_in.txt}
+        i=${i#pos_}
+        ./`cat func_tests/pos_${i}_args.txt` > output.txt
+        if [[ $? == 0 ]] && cmp -s "output.txt" "func_tests/pos_${i}_out.txt"
+        then
+            ((POSITIVES_SUCCEEDED++))
+        else
+            echo "Failed positive test $i. Program output:"
+            cat output.txt
+            echo
+        fi
+    fi
+    if [[ $file =~ neg_[0-9]{2}_in.txt ]]
+    then
+        ((NEGATIVES_SUM++))
+        i=${file%_in.txt}
+        i=${i#neg_}
+        ./`cat func_tests/neg_${i}_args.txt` > output.txt
+        if [[$? != 0]] && cmp -s "output.txt" "func_tests/neg_${i}_out.txt"
+        then
+            ((NEGATIVES_SUCCEEDED++))
+        else
+            echo "Failed negative test $i."
+            cat output.txt
+            echo
+        fi
+    fi
+done
+
+echo "Succeeded positives $POSITIVES_SUCCEEDED/$POSITIVES_SUM"
+echo "Succeeded negatives $NEGATIVES_SUCCEEDED/$NEGATIVES_SUM"
